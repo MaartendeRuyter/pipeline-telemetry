36abb11fcdf08aaf9cc0eaa4e2d892d4d9c15329
Maarten de Ruyter <5027985-MaartendeRuyter@users.noreply.gitlab.com>
2021-10-16 16:30:10 +0200

    return no errors test

--- tests/test_add_telemetry.py
+++ tests/test_add_telemetry.py
@@ -43,3 +43,16 @@ def test_validate_data_method():
                                       data={})
     assert ListErrors.KEY_NOT_FOUND.code in str(errors[0])
     assert '@KEY_<items>' in str(errors[0])
+
+
+def test_validate_data_method_returns_empty_list():
+    """
+    Test _validate_data_method returns empty list when no errors are found.
+    """
+    telemetry = Telemetry(telemetry_rules=TEST_TELEMETRY_RULES,
+                          **DEFAULT_TELEMETRY_PARAMS)
+    telemetry.increase_sub_process_base_count('RETRIEVE_RAW_DATA')
+    errors = telemetry._validate_data(sub_process='RETRIEVE_RAW_DATA',
+                                      data={'items': [1, 2, 3]})
+    assert errors == []
+
