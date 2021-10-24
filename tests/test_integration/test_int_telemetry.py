91eb6ebfec71a32eb1aad595a1c7765a7a583797
Maarten de Ruyter <5027985-MaartendeRuyter@users.noreply.gitlab.com>
2021-10-24 22:29:04 +0200

    added integration tests for entries_have_key validator

--- tests/test_integration/test_int_telemetry.py
+++ tests/test_integration/test_int_telemetry.py
@@ -2,7 +2,9 @@
 """
 from test_data import DEFAULT_TELEMETRY_PARAMS
 from test_integration_data import BASIC_RETRIEVE_DATA_TEST, \
-    MISSING_KEY_RETRIEVE_DATA_TEST, validate_result_from_telemetry
+    ITEMS_FIELD_HAS_DICT_TEST, MISSING_KEY_RETRIEVE_DATA_TEST, \
+    MUST_HAVE_KEY_IN_ITEMS_TEST, NO_ITEMS_HAVE_MUST_HAVE_KEY_TEST, \
+    validate_result_from_telemetry
 
 from pipeline_telemetry.main import Telemetry
 
@@ -31,3 +33,54 @@ def test_missing_key_integration_test():
     telemetry.save_and_close()
     assert validate_result_from_telemetry(
         telemetry, MISSING_KEY_RETRIEVE_DATA_TEST)
+
+
+def test_must_have_key_in_items_integration_test():
+    """
+    Test telemetry returns correct telemetry object when key is missing in one
+    of the items.
+    """
+    telemetry_rules = MUST_HAVE_KEY_IN_ITEMS_TEST.validation_rules
+    telemetry_params = DEFAULT_TELEMETRY_PARAMS | \
+        {'telemetry_rules': telemetry_rules}
+    telemetry = Telemetry(**telemetry_params)
+    telemetry.increase_sub_process_base_count('RETRIEVE_RAW_DATA')
+    telemetry.add('RETRIEVE_RAW_DATA',
+                  MUST_HAVE_KEY_IN_ITEMS_TEST.data, None)
+    telemetry.save_and_close()
+    assert validate_result_from_telemetry(
+        telemetry, MUST_HAVE_KEY_IN_ITEMS_TEST)
+
+
+def test_no_items_have_must_have_key_in_items_integration_test():
+    """
+    Test telemetry returns correct telemetry object when no item has the
+    correct key.
+    """
+    telemetry_rules = NO_ITEMS_HAVE_MUST_HAVE_KEY_TEST.validation_rules
+    telemetry_params = DEFAULT_TELEMETRY_PARAMS | \
+        {'telemetry_rules': telemetry_rules}
+    telemetry = Telemetry(**telemetry_params)
+    telemetry.increase_sub_process_base_count('RETRIEVE_RAW_DATA')
+    telemetry.add('RETRIEVE_RAW_DATA',
+                  NO_ITEMS_HAVE_MUST_HAVE_KEY_TEST.data, None)
+    telemetry.save_and_close()
+    assert validate_result_from_telemetry(
+        telemetry, NO_ITEMS_HAVE_MUST_HAVE_KEY_TEST)
+
+
+def test_items_has_dict_field_test():
+    """
+    Test telemetry returns correct telemetry object when items field contains
+    a dict field.
+    """
+    telemetry_rules = ITEMS_FIELD_HAS_DICT_TEST.validation_rules
+    telemetry_params = DEFAULT_TELEMETRY_PARAMS | \
+        {'telemetry_rules': telemetry_rules}
+    telemetry = Telemetry(**telemetry_params)
+    telemetry.increase_sub_process_base_count('RETRIEVE_RAW_DATA')
+    telemetry.add('RETRIEVE_RAW_DATA',
+                  ITEMS_FIELD_HAS_DICT_TEST.data, None)
+    telemetry.save_and_close()
+    assert validate_result_from_telemetry(
+        telemetry, ITEMS_FIELD_HAS_DICT_TEST)
