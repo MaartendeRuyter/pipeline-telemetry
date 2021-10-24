ec88bd71b05c1ea5b3f053f42dec662709cf7de1
Maarten de Ruyter <5027985-MaartendeRuyter@users.noreply.gitlab.com>
2021-10-24 14:29:06 +0200

    added validation errors for entries have key validator

--- src/pipeline_telemetry/settings/errors.py
+++ src/pipeline_telemetry/settings/errors.py
@@ -24,5 +24,21 @@ class ValidationErrors(FunctionalErrorsBaseClass):
         code='VALIDATE_ENTRIES_ERR_003',
         description='Field contains unexpected nr of items')
 
+    ENTRIES_FIELD_NOT_FOUND = ErrorCode(
+        code='ENTRIES_HAVE_KEY_ERR_001',
+        description='Entries field does not exist')
+
+    ENTRIES_FIELD_OF_WRONG_TYPE = ErrorCode(
+        code='ENTRIES_HAVE_KEY_ERR_002',
+        description='Entries field does not contain a list')
+
+    KEY_NOT_FOUND_IN_ENTRY = ErrorCode(
+        code='ENTRIES_HAVE_KEY_ERR_003',
+        description='Key missing in entry')
+
+    ENTRY_IS_NOT_A_DICT = ErrorCode(
+        code='ENTRIES_HAVE_KEY_ERR_004',
+        description='Entry that needs to have a key is not a dict')
+
 
 ListErrors.register_errors(ValidationErrors)
