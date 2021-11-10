84f353f1bc8edae33ed34f76fc1a13a6167bb6e1
Maarten de Ruyter <5027985-MaartendeRuyter@users.noreply.gitlab.com>
2021-11-10 11:21:50 +0100

    added telemetry decorator that uses mongo storage

--- src/pipeline_telemetry/helper.py
+++ src/pipeline_telemetry/helper.py
@@ -2,20 +2,24 @@
 """
 from typing import Any, Union
 
-from pipeline_telemetry.main import Telemetry
+from pipeline_telemetry.main import Telemetry, mongo_telemetry
 from pipeline_telemetry.settings.data_class import TelemetryCounter
 
 
 def add_telemetry(telemetry_params: dict) -> object:
     """
-    Decorator method to add a telemetry to the class from which the .
-    Class instance to which the method belongs should define a method
-    result_log_arguments returing a dict with keys:
-        reporting_process: process name
-        process_type: instance from enumerator ProcessTypes
-        process_class: class name of process
-        model_type: instance from enumerator ModelTypes
-        search_key_type: search key type of data processed
+    Decorator method to add a telemetry to the class from which the
+    decorator was called. When using this decorator the mongo storage
+    class will used.
+
+    Args:
+        telemetry_params (dict:
+            - process_name (str): free format process name
+            - process_type (ProcessType): process type definition
+            - telemetry_rules (dict): telemetry rules definition dict
+            - storage_class (AbstractTelemetryStorage, optional):
+                Storage class to be used to store telemetry instances. Defaults
+                to TelemetryInMemoryStorage which stores only in memory.
     """
 
     def wrapper(method):
@@ -25,6 +29,36 @@ def add_telemetry(telemetry_params: dict) -> object:
             """
             if (not hasattr(self, "_telemetry")) or (not self._telemetry):
                 self._telemetry = Telemetry(**telemetry_params)
+            result = method(self, *args, **kwargs)
+            self._telemetry.save_and_close()
+            self._telemetry = None
+            return result
+
+        return wrapped_method
+
+    return wrapper
+
+
+def add_mongo_telemetry(telemetry_params: dict) -> object:
+    """
+    Decorator method to add a telemetry to the class from which the
+    decorator was called. When using this decorator the mongo storage
+    class will used.
+
+    Args:
+        telemetry_params (dict:
+            - process_name (str): free format process name
+            - process_type (ProcessType): process type definition
+            - telemetry_rules (dict): telemetry rules definition dict
+    """
+
+    def wrapper(method):
+        def wrapped_method(self, *args, **kwargs):
+            """
+            Wrapper for method where result log should be added
+            """
+            if (not hasattr(self, "_telemetry")) or (not self._telemetry):
+                self._telemetry = mongo_telemetry(**telemetry_params)
             result = method(self, *args, **kwargs)
             self._telemetry.save_and_close()
             self._telemetry = None
