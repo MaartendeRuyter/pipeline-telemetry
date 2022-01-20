41817c6d1c595c48e40a74aab473008d6c14fade
Maarten de Ruyter <5027985-MaartendeRuyter@users.noreply.gitlab.com>
2022-01-20 11:55:22 +0100

    added test for telemetry type settings

---
+++ tests/test_settings_module.py
@@ -0,0 +1,15 @@
+"""Module to test settings module.
+"""
+from pipeline_telemetry.settings import settings
+
+
+def test_telemetry_type_settings():
+    """
+    Test that telemetry type settings is a dict where keys and values are
+    in uppercase and where underscores in the keys are replaced by spaces
+    in the values.
+    """
+    for k, v in settings.telemetry_types.items():
+        assert k == k.upper()
+        assert v == v.upper()
+        assert v == k.replace('_', ' ')
