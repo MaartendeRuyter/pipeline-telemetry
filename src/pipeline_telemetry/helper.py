a068e8369e7d9645add3e470d01a5518f789cf3f
Maarten de Ruyter <5027985-MaartendeRuyter@users.noreply.gitlab.com>
2022-01-31 15:01:44 +0100

    move decorator to helper module

--- src/pipeline_telemetry/helper.py
+++ src/pipeline_telemetry/helper.py
@@ -2,7 +2,9 @@
 """
 from typing import Any, Union
 
-from pipeline_telemetry.settings.data_class import TelemetryCounter
+from .settings import exceptions
+from .settings import settings as st
+from .settings.data_class import TelemetryCounter
 
 
 def is_telemetry_counter(counter: Union[TelemetryCounter, Any]) -> bool:
@@ -30,3 +32,26 @@ def increase_fail_count(
     object_with_telemetry._telemetry.increase_sub_process_fail_count(
         sub_process=sub_process, increment=increment
     )
+
+
+# decorator method to check status telemetry object
+def _raise_exception_if_telemetry_closed(method):
+    """
+    Decorator method to check if telemetry object is closed.
+    If so an exception is raised.
+
+    Decorator method to be used for methods that are only allowed
+    when telemetry object not yet closed.
+    """
+
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper to check if run_time has been set
+        If so the telemetry object is closed
+        """
+        if self.telemetry.get(st.RUN_TIME):
+            raise exceptions.TelemetryObjectAlreadyClosed()
+
+        return method(self, *args, **kwargs)
+
+    return wrapper
--- src/pipeline_telemetry/main.py
+++ src/pipeline_telemetry/main.py
@@ -18,30 +18,7 @@ from .settings import settings as st
 from .storage.generic import AbstractTelemetryStorage
 from .storage.memory import TelemetryInMemoryStorage
 from .validators.dict_validator import DictValidator
-
-
-# decorator method to check status telemetry object
-def _raise_exception_if_telemetry_closed(method):
-    """
-    Decorator method to check if telemetry object is closed.
-    If so an exception is raised.
-
-    Decorator method to be used for methods that are only allowed
-    when telemetry object not yet closed.
-    """
-
-    def wrapper(self, *args, **kwargs):
-        """
-        Wrapper to check if run_time has been set
-        If so the telemetry object is closed
-        """
-        if self.telemetry.get(st.RUN_TIME):
-            raise exceptions.TelemetryObjectAlreadyClosed()
-
-        return method(self, *args, **kwargs)
-
-    return wrapper
-
+from .helper import _raise_exception_if_telemetry_closed
 
 class Telemetry:
     """Class to manage the telemetry data object of a data pipeline process.
