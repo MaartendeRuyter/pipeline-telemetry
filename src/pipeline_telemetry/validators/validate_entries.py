d06998298cdf38038887fd3950bbde1c6275ff5f
Maarten de Ruyter <5027985-MaartendeRuyter@users.noreply.gitlab.com>
2022-08-30 09:32:41 +0200

    Added RuleData data class to imporove type checking and annotation

--- src/pipeline_telemetry/validators/validate_entries.py
+++ src/pipeline_telemetry/validators/validate_entries.py
@@ -1,58 +1,53 @@
 """Module to define validate entries validator class
 """
+from dataclasses import dataclass
+from typing import Dict, Optional
 import jmespath
 from errors import ErrorCode, ListErrors, add_error_data
 
 from ..settings import exceptions
 from ..validators.dict_validator import DictValidator
 
+from .abstract_validator_instruction import AbstractValidatorInstruction, \
+    BaseValidatorInstructionRuleData
 
-class ValidateEntries():
+
+@dataclass(frozen=True)
+class ValidateEntriesRuleData(BaseValidatorInstructionRuleData):
+    expected_count: int
+
+    def __post_init__(self):
+        if type(self.expected_count) is not int:
+            raise TypeError("Field 'expected_count' must be of type 'int'.")
+
+        if self.expected_count < 0:
+            raise exceptions.ExpectedCountMustBePositiveInt
+
+        super().__post_init__()
+
+
+class ValidateEntries(AbstractValidatorInstruction):
     """
     class to define validate entries instruction.
     This instruction counts the entries in a list or a dict and reports an
     error when the number of items in the list or dict is different from the
     expected nr of items
     """
-    instruction = 'validate_entries'
-    fieldname_field = 'field_name'
+    RULE_DATA_CLASS = ValidateEntriesRuleData
+    INSTRUCTION = 'validate_entries'
     expected_count_field = 'expected_count'
-
-    def __new__(cls):
-        """ make this a singleton class """
-        return cls
-
-    @classmethod
-    def validate(
-            cls, dict_to_validate: dict, rule_content: dict) -> list[ErrorCode]:
-        """
-        Public method run the validation
-        prior to validation the rule will be validated.
-
-        Args:
-            dict_to_validate (dict): Input dict that needs validation
-            rule_content (dict): actual rule
-
-        Returns:
-            list[ErrorCode]: List with errors that were found
-
-        Raise:
-            FieldNameMandatory: when field name not defined in rule
-        """
-        cls._validate_rule(rule_content)
-        return cls._validate(dict_to_validate, rule_content)
 
     @classmethod
     def _validate(
             cls, dict_to_validate: dict,
-            rule_content: dict) -> list[ErrorCode]:
+            rule_data: BaseValidatorInstructionRuleData) -> list[ErrorCode]:
         """
         method to do the actual validation
 
         returns:
             - error in case
         """
-        fieldname = cls._get_field_name(rule_content)
+        fieldname = cls._get_field_name(rule_data)
 
         field_to_validate = jmespath.search(fieldname, dict_to_validate)
 
@@ -64,34 +59,19 @@ class ValidateEntries():
             return cls._validation_error(
                 ListErrors.WRONG_TYPE_IN_FIELD, fieldname)
 
-        if len(field_to_validate) != cls._get_expected_count(rule_content):
+        if len(field_to_validate) != cls._get_expected_count(rule_data):
             return cls._validation_error(
                 ListErrors.UNEXPECTED_NR_OF_ITEMS, fieldname)
 
         return []
 
     @classmethod
-    def _validate_rule(cls, rule_content):
-        fieldname = cls._get_field_name(rule_content)
-        if not isinstance(fieldname, str):
-            raise exceptions.FieldNameMandatory(cls.instruction)
-        expected_count = cls._get_expected_count(rule_content)
-        if not isinstance(expected_count, int) or expected_count < 0:
-            raise exceptions.ExpectedCountMustBePositiveInt()
-
-    @classmethod
-    def _get_field_name(cls, rule_content):
-        """
-        retrieves the field name in scope from the rule content
-        """
-        return rule_content.get(cls.fieldname_field, None)
-
-    @classmethod
-    def _get_expected_count(cls, rule_content):
+    def _get_expected_count(cls, rule_data: BaseValidatorInstructionRuleData
+                            ) -> Optional[str]:
         """
         retrieves the expected_count in scope of the rule content
         """
-        return rule_content.get(cls.expected_count_field, None)
+        return getattr(rule_data, cls.expected_count_field)
 
     @staticmethod
     def _validation_error(
