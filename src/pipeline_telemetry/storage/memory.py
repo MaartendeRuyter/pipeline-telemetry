bd397f611b11c661ddd962e8e53cc1d146783384
Maarten de Ruyter <5027985-MaartendeRuyter@users.noreply.gitlab.com>
2022-09-18 21:22:19 +0200

    implememted telemetry_list method for in memory storage

--- src/pipeline_telemetry/storage/memory.py
+++ src/pipeline_telemetry/storage/memory.py
@@ -5,10 +5,25 @@ import sqlite3
 from datetime import datetime
 from typing import Dict, Iterator, Optional
 
+from pipeline_telemetry.data_classes.telemetry_models import TelemetryData
+
 from ..data_classes import TelemetryModel
 from ..settings import exceptions
 from ..settings import settings as st
 from .generic import AbstractTelemetryStorage
+
+
+def dict_factory(cursor, row):
+    """Method to allow sql queries to be returned as a dict.
+
+    The telemetry_data field is seperately converted to a dict because
+    sqlite returns a nested dict as a string.
+    """
+    col_names = [col[0] for col in cursor.description]
+    telemetry_data = {key: value for key, value in zip(col_names, row)}
+    telemetry_data_str = telemetry_data.pop('telemetry_data', "{}")
+    telemetry_data.update({'telemetry_data': json.loads(telemetry_data_str)})
+    return telemetry_data
 
 
 class TelemetryInMemoryStorage(AbstractTelemetryStorage):
@@ -30,6 +45,7 @@ class TelemetryInMemoryStorage(AbstractTelemetryStorage):
         """
         if not cls.db_in_memory:
             cls.db_in_memory = sqlite3.connect(":memory:")
+            cls.db_in_memory.row_factory = dict_factory
             cls.db_cursor = cls.db_in_memory.cursor()
             cls._define_db_table(cls.db_cursor)
 
@@ -54,7 +70,7 @@ class TelemetryInMemoryStorage(AbstractTelemetryStorage):
             CREATE TABLE telemetry (telemetry_type varchar(100),
             category varchar(60), sub_category varchar(60),
             source_name varchar(40), process_type varchar(40),
-            start_date_time timestamp, run_time varchar(20),
+            start_date_time timestamp, run_time_in_seconds varchar(20),
             telemetry_data json, traffic_light varchar(10),
             io_time_in_seconds real)"""
         )
@@ -93,7 +109,8 @@ class TelemetryInMemoryStorage(AbstractTelemetryStorage):
             source_name: str, process_type: str, from_date_time: datetime,
             to_date_time: datetime) -> Iterator:
         """
-        Select telemetry records unique to a single process and source for as specific time period.
+        Select telemetry records unique to a single process, source category
+        and sub category for as specific time period.
         """
         if not self.db_cursor:
             raise exceptions.StorageNotInitialized
@@ -109,3 +126,32 @@ class TelemetryInMemoryStorage(AbstractTelemetryStorage):
              f"start_date_time < '{to_date_time}'")
 
         return self.db_cursor.execute(select_statement)
+
+    @staticmethod
+    def _telemetry_storage_to_object(
+            stored_telemetry_object: Dict) -> TelemetryModel:
+        """Method to convert a sql light object into """
+        telemetry_data = stored_telemetry_object.pop('telemetry_data', "{}")
+        telemetry_model = TelemetryModel(**stored_telemetry_object)
+        for sub_process, sub_process_tel_data in telemetry_data.items():
+            telemetry_model.telemetry.update(
+                {sub_process: TelemetryData(**sub_process_tel_data)}
+            )
+
+        return telemetry_model
+
+    def telemetry_list(
+            self, telemetry_type: str, category: str, sub_category: str,
+            source_name: str, process_type: str, from_date_time: datetime,
+            to_date_time: datetime) -> Iterator[TelemetryModel]:
+        """
+        Method to return an iteraror TelemetryModel instances retrieved from a database query with the provided arguments.
+        """
+        selected_records = self.select_records(
+            telemetry_type=telemetry_type, category=category,
+            sub_category=sub_category, source_name=source_name,
+            process_type=process_type, from_date_time=from_date_time,
+            to_date_time=to_date_time
+        )
+        for record in selected_records:
+            yield self._telemetry_storage_to_object(record)
--- tests/test_storage/test_sqllite_storage.py
+++ tests/test_storage/test_sqllite_storage.py
@@ -1,5 +1,6 @@
 """Module to test storage module.
 """
+import pytest
 import sqlite3
 from datetime import datetime, timedelta
 
@@ -7,13 +8,15 @@ from freezegun import freeze_time
 from test_data import DEFAULT_TELEMETRY_MODEL_PARAMS
 
 from pipeline_telemetry.data_classes import TelemetryModel
+from pipeline_telemetry.settings import settings as st
+from pipeline_telemetry.settings import exceptions
 from pipeline_telemetry.storage.generic import AbstractTelemetryStorage
 from pipeline_telemetry.storage.memory import TelemetryInMemoryStorage
 
 
 def telemetry_query_params():
     """Returns a default set of query params"""
-    return  DEFAULT_TELEMETRY_MODEL_PARAMS | {
+    return DEFAULT_TELEMETRY_MODEL_PARAMS | {
         'from_date_time': datetime.now() - timedelta(days=1),
         'to_date_time': datetime.now() + timedelta(days=1)
     }
@@ -29,7 +32,7 @@ def test_in_memory_strorage_class_exists():
     assert TelemetryInMemoryStorage
 
 
-def test_in_memory_strorage_creates_db_in_memory():
+def test_in_memory_storage_creates_db_in_memory():
     """Test in memory storage instance has db_in_memory and db_cursor"""
     in_memory_storage = TelemetryInMemoryStorage()
     assert in_memory_storage.db_in_memory
@@ -37,6 +40,47 @@ def test_in_memory_strorage_creates_db_in_memory():
     assert isinstance(in_memory_storage.db_in_memory, sqlite3.Connection)
     assert isinstance(in_memory_storage.db_cursor, sqlite3.Cursor)
 
+
+def test_store_telemetry_raise_exception_when_db_is_closed():
+    """Test in memory storage instance has db_in_memory and db_cursor"""
+    in_memory_storage = TelemetryInMemoryStorage()
+    in_memory_storage.close_db()
+    with pytest.raises(exceptions.StorageNotInitialized):
+        in_memory_storage.store_telemetry(
+            TelemetryModel(**DEFAULT_TELEMETRY_MODEL_PARAMS))
+
+
+def test_select_records_raise_exception_when_db_is_closed():
+    """Test in memory storage instance has db_in_memory and db_cursor"""
+    in_memory_storage = TelemetryInMemoryStorage()
+    in_memory_storage.close_db()
+    with pytest.raises(exceptions.StorageNotInitialized):
+        in_memory_storage.select_records(
+            **telemetry_query_params())
+
+
+def test_close_db_in_memory():
+    """Test in memory storage close method close the db."""
+    in_memory_storage = TelemetryInMemoryStorage()
+    db_in_memory = in_memory_storage.db_in_memory
+    cursor = in_memory_storage.db_cursor
+    assert db_in_memory
+    assert cursor
+    in_memory_storage.close_db()
+    with pytest.raises(sqlite3.ProgrammingError):
+        cursor.execute("SELECT * FROM telemetry ")
+    with pytest.raises(sqlite3.ProgrammingError):
+        db_in_memory.cursor()
+
+
+def test_close_db_in_memory_can_be_closed_twice():
+    """
+    Test in memory storage close method can be called
+    when database is already closed.
+    """
+    in_memory_storage = TelemetryInMemoryStorage()
+    in_memory_storage.close_db()
+    assert in_memory_storage.close_db() is None
 
 def test_in_memory_strorage_is_only_created_once():
     """Test that once the database is created it will not be reinitialized"""
@@ -131,4 +175,80 @@ def test_store_telemetry_stores_object_with_create_date(mocker):
     in_memory_record = in_memory_storage.db_cursor.execute(
         "SELECT * FROM telemetry LIMIT 1"
     ).fetchone()
-    assert in_memory_record[5] == "2022-01-18 18:00:00.123456"
+    assert in_memory_record['start_date_time'] == "2022-01-18 18:00:00.123456"
+
+
+def test_telemetry_storage_to_object_returns_telemetry_model():
+    """
+    Test _telemetry_storage_to_object method returns a TelemetryModel instance
+    created from an in memory storage object.
+    """
+    # Table reset for each test is needed as the table is a class property
+    TelemetryInMemoryStorage._define_db_table(TelemetryInMemoryStorage.db_cursor)
+    in_memory_storage = TelemetryInMemoryStorage()
+    in_memory_storage.store_telemetry(
+        TelemetryModel(**DEFAULT_TELEMETRY_MODEL_PARAMS))
+    in_memory_record = in_memory_storage.db_cursor.execute(
+        "SELECT * FROM telemetry LIMIT 1").fetchone()
+    telemetry = in_memory_storage._telemetry_storage_to_object(
+        in_memory_record)
+    assert isinstance(telemetry, TelemetryModel)
+
+
+def test_telemetry_storage_to_object_returns_object_with_correct_values():
+    """
+    Test _telemetry_storage_to_object method returns a TelemetryModel instance
+    with the correct values.
+    """
+    # Table reset for each test is needed as the table is a class property
+    TelemetryInMemoryStorage._define_db_table(TelemetryInMemoryStorage.db_cursor)
+    in_memory_storage = TelemetryInMemoryStorage()
+    in_memory_storage.store_telemetry(
+        TelemetryModel(**DEFAULT_TELEMETRY_MODEL_PARAMS))
+    in_memory_record = in_memory_storage.db_cursor.execute(
+        "SELECT * FROM telemetry LIMIT 1").fetchone()
+    telemetry = in_memory_storage._telemetry_storage_to_object(
+        in_memory_record)
+    for key, value in DEFAULT_TELEMETRY_MODEL_PARAMS.items():
+        assert getattr(telemetry, key) == value
+
+
+def test_telemetry_storage_to_object_returns_object_with_telemetry_data():
+    """
+    Test _telemetry_storage_to_object method returns a TelemetryModel instance
+    with the correct values.
+    """
+    counter_key = 'TEST_COUNTER'
+    telemetry = TelemetryModel(**DEFAULT_TELEMETRY_MODEL_PARAMS)
+    sub_process = telemetry.get_sub_process_data('sub_process')
+    sub_process.increase_base_count(increment=1)
+    sub_process.increase_custom_count(increment=2, counter=counter_key)
+    # Table reset for each test is needed as the table is a class property
+    TelemetryInMemoryStorage._define_db_table(TelemetryInMemoryStorage.db_cursor)
+    in_memory_storage = TelemetryInMemoryStorage()
+    in_memory_storage.store_telemetry(telemetry)
+    in_memory_record = in_memory_storage.db_cursor.execute(
+        "SELECT * FROM telemetry LIMIT 1").fetchone()
+    telemetry_model = in_memory_storage._telemetry_storage_to_object(
+        in_memory_record)
+    telemetry_data = telemetry_model.get_sub_process_data(
+        'sub_process')
+    assert telemetry_data
+    assert getattr(telemetry_data, st.BASE_COUNT_KEY) == 1
+    assert getattr(telemetry_data, st.COUNTERS_KEY)[counter_key] == 2
+
+
+def test_telemetry_list_returns_list_with_telemetry_model_instances():
+    """
+    Test telemetry_list returns a list with TelemetryModel instances.
+    """
+    # Table reset for each test is needed as the table is a class property
+    TelemetryInMemoryStorage._define_db_table(TelemetryInMemoryStorage.db_cursor)
+    telemetry = TelemetryModel(**DEFAULT_TELEMETRY_MODEL_PARAMS)
+    in_memory_storage = TelemetryInMemoryStorage()
+    in_memory_storage.store_telemetry(telemetry)
+    in_memory_storage.store_telemetry(telemetry)
+    list_of_telemetry_models = in_memory_storage.telemetry_list(
+        **telemetry_query_params())
+    for telemetry_model in list_of_telemetry_models:
+        assert isinstance(telemetry_model, TelemetryModel)
