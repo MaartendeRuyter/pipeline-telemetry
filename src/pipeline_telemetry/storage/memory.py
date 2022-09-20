de864198b41002dfb594cb332a05b4777a4117cf
Maarten de Ruyter <5027985-MaartendeRuyter@users.noreply.gitlab.com>
2022-09-20 11:44:42 +0200

    removed method defined in generic.
    Change property telemetry_data into telemetry.
    This in order to be in line with mongo storage

--- src/pipeline_telemetry/storage/memory.py
+++ src/pipeline_telemetry/storage/memory.py
@@ -3,9 +3,7 @@
 import json
 import sqlite3
 from datetime import datetime
-from typing import Dict, Iterator, Optional
-
-from pipeline_telemetry.data_classes.telemetry_models import TelemetryData
+from typing import Iterator, Optional
 
 from ..data_classes import TelemetryModel
 from ..settings import exceptions
@@ -16,21 +14,20 @@ from .generic import AbstractTelemetryStorage
 def dict_factory(cursor, row):
     """Method to allow sql queries to be returned as a dict.
 
-    The telemetry_data field is seperately converted to a dict because
+    The telemetry field is seperately converted to a dict because
     sqlite returns a nested dict as a string.
     """
     col_names = [col[0] for col in cursor.description]
-    telemetry_data = {key: value for key, value in zip(col_names, row)}
-    telemetry_data_str = telemetry_data.pop('telemetry_data', "{}")
-    telemetry_data.update({'telemetry_data': json.loads(telemetry_data_str)})
-    return telemetry_data
+    telemetry = {key: value for key, value in zip(col_names, row)}
+    telemetry_str = telemetry.pop('telemetry', "{}")
+    telemetry.update({'telemetry': json.loads(telemetry_str)})
+    return telemetry
 
 
 class TelemetryInMemoryStorage(AbstractTelemetryStorage):
     """
     Class to provice telemetry in memory storage for use when unit testing
     """
-
     db_in_memory: Optional[sqlite3.Connection] = None
     db_cursor: Optional[sqlite3.Cursor] = None
 
@@ -71,7 +68,7 @@ class TelemetryInMemoryStorage(AbstractTelemetryStorage):
             category varchar(60), sub_category varchar(60),
             source_name varchar(40), process_type varchar(40),
             start_date_time timestamp, run_time_in_seconds varchar(20),
-            telemetry_data json, traffic_light varchar(10),
+            telemetry json, traffic_light varchar(10),
             io_time_in_seconds real)"""
         )
 
@@ -89,18 +86,18 @@ class TelemetryInMemoryStorage(AbstractTelemetryStorage):
         run_time_in_seconds = getattr(telemetry, st.RUN_TIME)
         traffic_light = getattr(telemetry, st.TRAFFIC_LIGHT_KEY)
         io_time_in_seconds = getattr(telemetry, st.IO_TIME_KEY)
-        telemetry_data = {
+        telemetry_dict = {
             k: v.__dict__ for k, v in
             getattr(telemetry, st.TELEMETRY_FIELD_KEY).items()
         }
-        json_object = json.dumps(telemetry_data)
+        telemetry_json = json.dumps(telemetry_dict)
 
         self.db_cursor.execute(
             "insert into telemetry values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
             [
                 telemetry_type, category, sub_category,
                 source_name, process_type, start_date_time,
-                str(run_time_in_seconds), json_object,
+                str(run_time_in_seconds), telemetry_json,
                 traffic_light, str(io_time_in_seconds)
             ])
 
@@ -126,32 +123,3 @@ class TelemetryInMemoryStorage(AbstractTelemetryStorage):
              f"start_date_time < '{to_date_time}'")
 
         return self.db_cursor.execute(select_statement)
-
-    @staticmethod
-    def _telemetry_storage_to_object(
-            stored_telemetry_object: Dict) -> TelemetryModel:
-        """Method to convert a sql light object into """
-        telemetry_data = stored_telemetry_object.pop('telemetry_data', "{}")
-        telemetry_model = TelemetryModel(**stored_telemetry_object)
-        for sub_process, sub_process_tel_data in telemetry_data.items():
-            telemetry_model.telemetry.update(
-                {sub_process: TelemetryData(**sub_process_tel_data)}
-            )
-
-        return telemetry_model
-
-    def telemetry_list(
-            self, telemetry_type: str, category: str, sub_category: str,
-            source_name: str, process_type: str, from_date_time: datetime,
-            to_date_time: datetime) -> Iterator[TelemetryModel]:
-        """
-        Method to return an iteraror TelemetryModel instances retrieved from a database query with the provided arguments.
-        """
-        selected_records = self.select_records(
-            telemetry_type=telemetry_type, category=category,
-            sub_category=sub_category, source_name=source_name,
-            process_type=process_type, from_date_time=from_date_time,
-            to_date_time=to_date_time
-        )
-        for record in selected_records:
-            yield self._telemetry_storage_to_object(record)
